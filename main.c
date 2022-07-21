b05521bc2df6b5a10a020055705ade0b019a3f29
mattyred <mattiarosso99@gmail.com>
2022-07-21 21:05:33 +0200

    Tests on execution time

--- main.c
+++ main.c
@@ -11,9 +11,15 @@ int main(int argc, char* argv[]) {
     FILE* fin;
     struct timespec begin, end;
     
-    //G = GRAPHload_sequential(argv[1]);
     //GRAPHstore(G, stdout);
-    G = GRAPHload_parallel3(argv[1], 10000, 20, 20);
+    clock_gettime(CLOCK_REALTIME, &begin);
+    //G = GRAPHload_sequential(argv[1]);
+    G = GRAPHload_parallel3(argv[1], 1000, 5, 5);
+    clock_gettime(CLOCK_REALTIME, &end);
+    long seconds = end.tv_sec - begin.tv_sec;
+    long nanoseconds = end.tv_nsec - begin.tv_nsec;
+    double elapsed = seconds + nanoseconds*1e-9;
+    printf("Time measured: %.9f seconds.\n", elapsed);
     //G= GRAPHload_sequential(argv[1]);
     // GRAPHstore(G, stdout);
     /*
