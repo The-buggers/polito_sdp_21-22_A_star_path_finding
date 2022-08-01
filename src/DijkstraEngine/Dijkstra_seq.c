7c3bb6c8dd609205f72821415416ecd9b3684255
mattyred <mattiarosso99@gmail.com>
2022-08-01 20:38:58 +0200

    Dijkstra not working

--- src/DijkstraEngine/Dijkstra_seq.c
+++ src/DijkstraEngine/Dijkstra_seq.c
@@ -1,25 +1,25 @@
 #include "Dijkstra.h"
 #include "float.h"
 #define maxWT DBL_MAX
-void DIJKSTRA_shortest_path_sequential(Graph G, int source, int dest){
+void DIJKSTRA_shortest_path_sequential(Graph G, int source, int dest) {
     int v;
     link t;
     int V = GRAPHget_num_nodes(G);
     PQ pq = PQinit(V);
-    int *st;
+    int *parentVertex;
     double *mindist;
     int a, b;
     double a_b_wt;
-    st = malloc(V * sizeof(int));
+    parentVertex = malloc(V * sizeof(int));
     mindist = malloc(V * sizeof(double));
-    if ((st == NULL) || (mindist == NULL)) return;
+    if ((parentVertex == NULL) || (mindist == NULL)) return;
     for (v = 0; v < V; v++) {
-        st[v] = -1;
+        parentVertex[v] = -1;
         mindist[v] = maxWT;
         PQinsert(pq, mindist, v);
     }
     mindist[source] = 0;
-    st[source] = source;
+    parentVertex[source] = -1;
     PQchange(pq, mindist, source);
     while (!PQempty(pq)) {
         if (mindist[a = PQextractMin(pq, mindist)] != maxWT) {
@@ -30,13 +30,13 @@ void DIJKSTRA_shortest_path_sequential(Graph G, int source, int dest){
                 if (mindist[a] + a_b_wt < mindist[b]) {
                     mindist[b] = mindist[a] + a_b_wt;
                     PQchange(pq, mindist, b);
-                    st[b] = a;
+                    parentVertex[b] = a;
                 }
             }
+            // if (a == dest) break;
         }
-        
-        reconstruct_path(st, source, dest, mindist);
-        PQfree(pq);
     }
+    reconstruct_path(parentVertex, source, dest, mindist);
+    PQfree(pq);
 }
 // ^(\s)*$\n
