fde8ad3a80b8d724a8608c1e4513d139e9a3da07
mattyred <mattiarosso99@gmail.com>
2022-08-08 23:01:06 +0200

    Changed readme

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 1. cd RandomGraphGenerator
 2. gcc -Wall RandomGraphGenerator.c -o randgen -lm
 3. ./randgen \[Path\] \[mode 0/1 \] \[max_x\] \[max_y\] \[V\] \[max_k\]
+
+## HPC Jupyter PoliTO
+1. cd ~/ParallelAstarProject/polito_sdp_21-22_A_star_path_finding/src 
+2. Check that the path of each statistics file is : "../stats/name" (in VM it is only "./stats/name" because src is the root)
+3. make clean; make target
+4. cd build
+5. ./graphtest ../../Benchmark/DIMACS_custom_format/binarydUSA-road-dBAY 321269 263446 2 h
--- src/main.c
+++ src/main.c
@@ -73,12 +73,12 @@ int main(int argc, char* argv[]) {
     printf("Reading time: %.9f seconds\n\n", stop_timer(begin));
 
     start_timer(&begin);
-    //ASTARshortest_path_sequential(G, source, dest, heuristic_type);
+    ASTARshortest_path_sequential(G, source, dest, heuristic_type);
     //ASTARshortest_path_sas_sf(G, source, dest, heuristic_type, num_threads);
     //ASTARshortest_path_sas_b(G, source, dest, heuristic_type, num_threads);
     //ASTARshortest_path_fa(G, source, dest, heuristic_type, num_threads);
     //ASTARshortest_path_mp(G, source, dest, heuristic_type, num_threads);
-    DIJKSTRA_shortest_path_sequential(G, source, dest);
+    //DIJKSTRA_shortest_path_sequential(G, source, dest);
     printf("A* algorithm time: %.9f seconds\n", stop_timer(begin));
     return 0;
 }
