3e0a011e1c306e9d49fc2c30cc5e9a58965b4aee
ippo <s295502@studenti.polito.it>
2022-08-30 11:18:21 +0200

    implementing pruning

--- src/AstarEngine/Astar_ab_ba.c
+++ src/AstarEngine/Astar_ab_ba.c
@@ -23,6 +23,10 @@ struct arg_t {
     int *parentVertex;
     double *gvalues;
     double *otherGvalues;
+    double *hvalues;
+    double *otherHvalues;
+    double *F;
+    double *otherF;
     int *M;
     int *found;
     int *L;
@@ -32,14 +36,14 @@ struct arg_t {
     int *expanded_nodes;
 #endif
 };
-
+pthread_barrier_t barr;
 static void *nba(void *arg);
 
 static void *nba(void *arg) {
     struct arg_t *args = (struct arg_t *)arg;
     int i, v, a, b;
     double f_extracted_node, g_b, f_b, a_b_wt;
-    double *fvalues, *hvalues;
+    double *fvalues;
     Position p;
     link t;
     // - Insert all the nodes in the priority queue
@@ -49,17 +53,19 @@ static void *nba(void *arg) {
 
     PQ open_list = PQinit(args->V);
     fvalues = (double *)malloc(args->V * sizeof(double));
-    hvalues = (double *)malloc(args->V * sizeof(double));
-    if ((fvalues == NULL) || (hvalues == NULL)) return;
+    // hvalues = (double *)malloc(args->V * sizeof(double));
+    if (fvalues == NULL) return;
     for (v = 0; v < args->V; v++) {
         p = GRAPHget_node_position(args->G, v);
-        hvalues[v] = heuristic(p, args->pos_dest, args->heuristic_type);
+        args->hvalues[v] = heuristic(p, args->pos_dest, args->heuristic_type);
         fvalues[v] = maxWT;
     }
     fvalues[args->source] =
-        compute_f(hvalues[args->source], 0);  // g(n) = 0 for n == source
+        compute_f(args->hvalues[args->source], 0);  // g(n) = 0 for n == source
     args->gvalues[args->source] = 0;
     PQinsert(open_list, fvalues, args->source);
+
+    pthread_barrier_wait(&barr);
 
     while (!PQempty(open_list))  // while OPEN list not empty
     {
@@ -71,39 +77,44 @@ static void *nba(void *arg) {
 
         if (args->M[a] == 1) {
             // For each successor 'b' of node 'a':
-            for (t = GRAPHget_list_node_head(args->G, a);
-                 t != GRAPHget_list_node_tail(args->G, a);
-                 t = LINKget_next(t)) {
-                b = LINKget_node(t);
-                a_b_wt = LINKget_wt(t);
-                // Compute f(b) = g(b) + h(b) = [g(a) + w(a,b)] + h(b)
-                g_b = args->gvalues[a] + a_b_wt;
-                f_b = g_b + hvalues[b];
+            if ((fvalues[a] < *(args->L)) ||
+                (args->gvalues[a] + *(args->otherF) - args->otherHvalues[a] <
+                 *(args->L))) {
+                for (t = GRAPHget_list_node_head(args->G, a);
+                     t != GRAPHget_list_node_tail(args->G, a);
+                     t = LINKget_next(t)) {
+                    b = LINKget_node(t);
+                    a_b_wt = LINKget_wt(t);
+                    // Compute f(b) = g(b) + h(b) = [g(a) + w(a,b)] + h(b)
+                    g_b = args->gvalues[a] + a_b_wt;
+                    f_b = g_b + args->hvalues[b];
 
-                if (args->M[b] == 1) {
-                    if (g_b < args->gvalues[b]) {
-                        args->parentVertex[b] = a;
-                        args->costToCome[b] = a_b_wt;
-                        args->gvalues[b] = g_b;
-                        fvalues[b] = f_b;
-                        if (PQsearch(open_list, b) == -1) {
-                            PQinsert(open_list, fvalues, b);
-                        } else
-                            PQchange(open_list, fvalues, b);
+                    if (args->M[b] == 1 &&
+                        args->gvalues[b] > args->gvalues[a] + a_b_wt) {
+                        if (g_b < args->gvalues[b]) {
+                            args->parentVertex[b] = a;
+                            args->costToCome[b] = a_b_wt;
+                            args->gvalues[b] = g_b;
+                            fvalues[b] = f_b;
+                            if (PQsearch(open_list, b) == -1) {
+                                PQinsert(open_list, fvalues, b);
+                            } else
+                                PQchange(open_list, fvalues, b);
 
-                        if ((args->gvalues[b] < maxWT &&
-                             args->otherGvalues[b] < maxWT) &&
-                            (*(args->L) >
-                             (args->gvalues[b] + args->otherGvalues[b]))) {
                             if ((args->gvalues[b] < maxWT &&
                                  args->otherGvalues[b] < maxWT) &&
                                 (*(args->L) >
                                  (args->gvalues[b] + args->otherGvalues[b]))) {
-                                pthread_spin_lock(args->m);
-                                *(args->common_pos) = b;
-                                *(args->L) =
-                                    (args->gvalues[b] + args->otherGvalues[b]);
-                                pthread_spin_unlock(args->m);
+                                if ((args->gvalues[b] < maxWT &&
+                                     args->otherGvalues[b] < maxWT) &&
+                                    (*(args->L) > (args->gvalues[b] +
+                                                   args->otherGvalues[b]))) {
+                                    pthread_spin_lock(args->m);
+                                    *(args->common_pos) = b;
+                                    *(args->L) = (args->gvalues[b] +
+                                                  args->otherGvalues[b]);
+                                    pthread_spin_unlock(args->m);
+                                }
                             }
                         }
                     }
@@ -113,6 +124,7 @@ static void *nba(void *arg) {
             args->M[a] = 0;
             pthread_spin_unlock(args->m);
         }
+        *(args->F) = fvalues[PQshowMin(open_list)];
         if (*(args->found) > 0) break;
     }
     *(args->found) = 1;
@@ -126,7 +138,8 @@ void ASTARshortest_path_ab_ba(Graph G, Graph R, int source, int dest,
     int V = GRAPHget_num_nodes(G);
     int v, i, *parentVertexG, *parentVertexR, *M, common_pos = -1, found = 0,
                                                   L = maxWT;
-    double *costToComeG, *costToComeR, *gvaluesG, *gvaluesR;
+    double *costToComeG, *costToComeR, *gvaluesG, *gvaluesR, *hvaluesG,
+        *hvaluesR, FG = maxWT, FR = maxWT;
     Position pos_source = GRAPHget_node_position(G, source);
     Position pos_dest = GRAPHget_node_position(G, dest);
     pthread_t *threads;
@@ -144,6 +157,8 @@ void ASTARshortest_path_ab_ba(Graph G, Graph R, int source, int dest,
     costToComeR = (double *)malloc(V * sizeof(double));
     gvaluesG = (double *)malloc(V * sizeof(double));
     gvaluesR = (double *)malloc(V * sizeof(double));
+    hvaluesG = (double *)malloc(V * sizeof(double));
+    hvaluesR = (double *)malloc(V * sizeof(double));
     M = (int *)malloc(V * sizeof(int));
 #if COLLECT_STAT
     int *expanded_nodes = (int *)calloc(V, sizeof(int));
@@ -161,7 +176,7 @@ void ASTARshortest_path_ab_ba(Graph G, Graph R, int source, int dest,
     }
 
     pthread_spin_init(&m, 0);
-
+    pthread_barrier_init(&barr, NULL, 2);
     for (i = 0; i < N; i++) {
         if (i == 0) {
             args[i].G = G;
@@ -171,6 +186,10 @@ void ASTARshortest_path_ab_ba(Graph G, Graph R, int source, int dest,
             args[i].parentVertex = parentVertexG;
             args[i].gvalues = gvaluesG;
             args[i].otherGvalues = gvaluesR;
+            args[i].hvalues = hvaluesG;
+            args[i].otherHvalues = hvaluesG;
+            args[i].F = &FG;
+            args[i].otherF = &FR;
             args[i].costToCome = costToComeG;
         } else {
             args[i].G = R;
@@ -180,6 +199,10 @@ void ASTARshortest_path_ab_ba(Graph G, Graph R, int source, int dest,
             args[i].parentVertex = parentVertexR;
             args[i].gvalues = gvaluesR;
             args[i].otherGvalues = gvaluesG;
+            args[i].hvalues = hvaluesR;
+            args[i].otherHvalues = hvaluesG;
+            args[i].F = &FR;
+            args[i].otherF = &FG;
             args[i].costToCome = costToComeR;
         }
         args[i].index = i;
@@ -211,7 +234,7 @@ void ASTARshortest_path_ab_ba(Graph G, Graph R, int source, int dest,
     }
 #if COLLECT_STAT
     int n = 0, tot = 0;
-    FILE *fp = fopen("./stats/stat_astar_seq.txt", "w+");
+    FILE *fp = fopen("./stats/stat_astar_ab_ba.txt", "w+");
     for (v = 0; v < V; v++) {
         if (expanded_nodes[v] != 0) {
             n++;
